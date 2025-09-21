421650d5cd1e4d0b4cf3d863919df9d9b8f9c5cf
Lucas Minville <lucas_minville@hotmail.com>
2025-09-21 15:24:25 -0400

    ajout installation.js

--- scripts/installation.js
+++ scripts/installation.js
@@ -1,40 +1,25 @@
-let deferredInstallPrompt = null;
-const installButton = document.getElementById("butInstall");
-installButton.addEventListener("click", installPWA);
-// Add event listener for beforeinstallprompt event
-window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
+let deferredPrompt;
 
-function saveBeforeInstallPromptEvent(evt) {
-  // CODELAB: Add code to save event & show the install button.
-  deferredInstallPrompt = evt;
-  installButton.removeAttribute("hidden");
-}
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
 
-function installPWA(evt) {
-  // Add code show install prompt & hide the install button.
-  deferredInstallPrompt.prompt();
-  // Hide the install button, it can't be called twice.
-  evt.srcElement.setAttribute("hidden", true);
-  // Log user response to prompt.
-  deferredInstallPrompt.userChoice.then((choice) => {
-    if (choice.outcome === "accepted") {
-      console.log("User accepted the A2HS prompt", choice);
-    } else {
-      console.log("User dismissed the A2HS prompt", choice);
-    }
-    deferredInstallPrompt = null;
+  const installBtn = document.getElementById("installBtn");
+  installBtn.style.display = "block";
+
+  installBtn.addEventListener("click", async () => {
+    installBtn.style.display = "none";
+
+    deferredPrompt.prompt();
+
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`Résultat : ${outcome}`);
+
+    deferredPrompt = null;
   });
-}
+});
 
-// Add event listener for appinstalled event
-window.addEventListener("appinstalled", logAppInstalled);
-/**
- * Event handler for appinstalled event.
- * Log the installation to analytics or save the event somehow.
- *
- * @param {Event} evt
- */
-function logAppInstalled(evt) {
-  // Add code to log the event
-  console.log("Weather App was installed.", evt);
-}
+window.addEventListener("appinstalled", () => {
+  console.log("PWA installée");
+  document.getElementById("installBtn").style.display = "none";
+});
